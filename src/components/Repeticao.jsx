8c2ac5ff1789a79529bfe3a2e44ce5fac9c4d013
Wesley Westelley <wesleywestelley@gmail.com>
2021-07-27 21:01:37 -0300

    :wrench: Exemplo de repetição no react

---
+++ src/components/Repeticao.jsx
@@ -0,0 +1,25 @@
+import products from '../components/data/products'
+import React from 'react'
+
+export default function Repeticao(props) {
+
+    function getProdutosLListItem() {
+        return products.map((prod) => {
+            return (
+
+                <li key={prod.id}>
+                    {prod.id} - {prod.name} - R$ {prod.price}
+                </li>
+            )
+        })
+    }
+
+    return (
+        <div>
+            <h2>Repetição</h2>
+            <ul>
+                {getProdutosLListItem()}
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/data/products.js
@@ -0,0 +1,8 @@
+
+const Items = [
+    {id:1, name: 'Caneta', price: 7.59},
+    {id:2, name: 'Lapís', price: 3.89},
+    {id:3, name: 'Caderno', price:18.30},
+]
+
+export default Items
